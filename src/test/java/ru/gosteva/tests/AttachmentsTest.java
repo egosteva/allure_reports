1c98fc0476f684d9b7fd243d32d83757501d152c
egosteva <elenagosteva.qa@gmail.com>
2023-04-04 23:41:26 +0300

    del imports

--- src/test/java/ru/gosteva/tests/AttachmentsTest.java
+++ src/test/java/ru/gosteva/tests/AttachmentsTest.java
@@ -1,15 +1,13 @@
 package ru.gosteva.tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.webdriver;
 import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
-import static org.openqa.selenium.By.linkText;
 
 public class AttachmentsTest {
 
